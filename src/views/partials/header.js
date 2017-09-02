9265ecb198743da5bcdf1b1cfd3ad90a8a158620
Terra Bilderback <terra@itson.me>
2017-09-01 17:10:08 -0700

    Added (temporary) icons; removed placeholder ItsOnMe text

--- src/views/partials/header.js
+++ src/views/partials/header.js
@@ -7,20 +7,20 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <div className="logo">
-          ItsOnMe
-        </div>
+        <div className="logo"></div>
         <nav>
           <Link to="/venue" className="item">
-            <img src="nav-icon-venue.png" />
+            <img src="https://cdn4.iconfinder.com/data/icons/shopping-52/24/Gift-Card-128.png" />
             <span>Venue</span>
           </Link>
+          <hr/>
           <Link to="/photos" className="item">
-            <img src="nav-icon-photos.png" />
+            <img src="http://www.freeiconspng.com/uploads/pictures-icon-22.gif" />
             <span>Photos</span>
           </Link>
+          <hr/>
           <Link to="/checkout" className="item">
-            <img src="nav-icon-checkout.png" />
+            <img src="https://d30y9cdsu7xlg0.cloudfront.net/png/210124-200.png" />
             <span>Checkout</span>
           </Link>
         </nav>
