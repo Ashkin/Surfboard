d90650c8198ca09909b2a284d935266d46a1cea3
Terra Bilderback <terra@itson.me>
2017-09-06 18:03:39 -0700

    Redux-controlled creditcard form

--- src/components/creditcard-form.js
+++ src/components/creditcard-form.js
@@ -3,39 +3,40 @@ import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
+import classBuilder           from '../helpers/class-builder'
+import { saveCreditcardData } from '../actions'
+
 
 class CreditcardForm extends Component {
 
   renderField(field) {
-    const { placeholder, required, handleChange, input: { name } } = field
+    const { placeholder, required, handleChange, getValue, input: { name } } = field
     const { touched, error } = field.meta
 
     const hasError = (touched && error)
-
-    console.log("[CreditcardForm::renderField]  handleChange: ", handleChange)
 
     // className builder
     let  classes = [field.className]
     if (hasError) classes.push('error')
     classes = classes.join(" ")
 
+
     return (
       <input
         className={classes}
         type={field.type || "text"}
         placeholder={placeholder}
-        onChange={event => { handleChange(name, event.target.value) }}
         {...field.input}
+        value={getValue(name)}
+        onChange={event => { handleChange(name, event.target.value) }}
       />
     )
   }
 
 
 
-  handleChange(name, newVal) {
-    console.log("[CreditcardForm::handleChange]")
-    console.log(" | name:   ", name)
-    console.log(" | newVal: ", newVal)
+  handleChange(name, val) {
+    this.props.saveCreditcardData({[name]: val})
   }
 
   onSubmit(values) {
@@ -44,6 +45,13 @@ class CreditcardForm extends Component {
   }
 
 
+  getCreditcardValue(key) {
+    // State isn't populated yet
+    if (this.props == null)  return "ohnoes"
+
+    return this.props.creditcard[key] || ''
+  }
+
 
   render() {
     const { handleSubmit, submitFailed} = this.props  // Magic.  comes from redux-form
@@ -51,9 +59,11 @@ class CreditcardForm extends Component {
     const errorMessage = (submitFailed ? "Please fill out the missing fields above." : "")
     const handleChange = this.handleChange.bind(this)
 
+    const getCreditcardValue = this.getCreditcardValue.bind(this)
+
 
     return (
-      <section className="creditcard-form">
+      <section className={classBuilder("creditcard-form", this.props.className)}>
         <header>
           <span className="filled-circle">2</span> Billing
         </header>
@@ -61,21 +71,16 @@ class CreditcardForm extends Component {
           Credit Card Information
         </summary>
         <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-          <Field component={this.renderField}  handleChange={handleChange}  name="name"        placeholder="Name on Card"        className="creditcard-name"   />
-          <Field component={this.renderField}  handleChange={handleChange}  name="cardnumber"  placeholder="Credit Card Number"  className="creditcard-number" />
-          <Field component={this.renderField}  handleChange={handleChange}  name="cvv"         placeholder="CVV"                 className="cvv"               />
-          <Field component={this.renderField}  handleChange={handleChange}  name="exp_mo"      placeholder="Exp. Month"          className="expiration"        />
-          <Field component={this.renderField}  handleChange={handleChange}  name="exp_yr"      placeholder="Exp. Year"           className="expiration"        />
-          <Field component={this.renderField}  handleChange={handleChange}  name="zip"         placeholder="Zip Code"            className="zip"               />
+          <Field component={this.renderField}  handleChange={handleChange}  name="name"    getValue={getCreditcardValue}  placeholder="Name on Card"        className="creditcard-name"   />
+          <Field component={this.renderField}  handleChange={handleChange}  name="number"  getValue={getCreditcardValue}  placeholder="Credit Card Number"  className="creditcard-number" />
+          <Field component={this.renderField}  handleChange={handleChange}  name="cvv"     getValue={getCreditcardValue}  placeholder="CVV"                 className="cvv"               />
+          <Field component={this.renderField}  handleChange={handleChange}  name="exp_mo"  getValue={getCreditcardValue}  placeholder="Exp. Month"          className="expiration"        />
+          <Field component={this.renderField}  handleChange={handleChange}  name="exp_yr"  getValue={getCreditcardValue}  placeholder="Exp. Year"           className="expiration"        />
+          <Field component={this.renderField}  handleChange={handleChange}  name="zip"     getValue={getCreditcardValue}  placeholder="Zip Code"            className="zip"               />
 
           <br className="clearfix" />
 
           <div className="error">{errorMessage}</div>
-
-          <div className="center">
-            <button type="submit" className="button">Test Submit</button>
-          </div>
-
         </form>
       </section>
     )
@@ -91,12 +96,11 @@ function addError(errors, key, message) {
 function validate(values) {
   const errors = {}
 
-  if (!values.name)       addError(errors, "name",       "Please enter the cardholder name")
-  if (!values.cardnumber) addError(errors, "cardnumber", "Please enter a card number")
-  if (!values.cvv)        addError(errors, "cvv",        "Missing CVV")
-  if (!values.exp_mo)     addError(errors, "exp_mo",     "Missing expiration month")
-  if (!values.exp_yr)     addError(errors, "exp_yr",     "Missing expiration year")
-
+  if (!values.name)    addError(errors, "name",    "Please enter the cardholder name")
+  if (!values.number)  addError(errors, "number",  "Please enter a card number")
+  if (!values.cvv)     addError(errors, "cvv",     "Missing CVV")
+  if (!values.exp_mo)  addError(errors, "exp_mo",  "Missing expiration month")
+  if (!values.exp_yr)  addError(errors, "exp_yr",  "Missing expiration year")
 
   return errors
 }
@@ -104,10 +108,8 @@ function validate(values) {
 
 
 function mapStateToProps(state) {
-  const newState = { initialValues:  state.form.CreditcardForm.values }
-
-  console.log("[CreditcardForm::mapStateToProps]  newState:", newState)
-  return newState
+  const creditcard = state.creditcard || {}
+  return { creditcard }
 }
 
 
@@ -118,5 +120,5 @@ export default reduxForm({
   validate,
   form: 'CreditcardForm'
 })(
-  connect(mapStateToProps,{  })(CreditcardForm)
+  connect(mapStateToProps,{ saveCreditcardData })(CreditcardForm)
 )
