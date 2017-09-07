b753d12d9cae416bf5405979183f38ff9854071d
Terra Bilderback <terra@itson.me>
2017-09-06 17:49:09 -0700

    Added a classBuilder helper

---
+++ src/helpers/class-builder.js
@@ -0,0 +1,7 @@
+
+// builds a class list, discarding null/undefined values
+export default function() {
+  return [...arguments].map(arg => {
+    return (arg != null ? arg : '')
+  }).join(" ").trim()
+}
