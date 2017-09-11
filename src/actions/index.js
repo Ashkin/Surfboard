d26306564613385501ba50b9fc6d3d60f4b20e23
Terra Bilderback <terra@itson.me>
2017-09-11 13:07:55 -0700

    Reordered actions to match UX flow

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
 export const ACTIONS = {
-  SELECT_PLAN: 'SELECT_PLAN',
+  UPDATE_VENUE:        'UPDATE_VENUE',
+  UPDATE_CONTACT:      'UPDATE_CONTACT',
+  SELECT_PLAN:         'SELECT_PLAN',
   UPDATE_STRIPE_TOKEN: 'UPDATE_STRIPE_TOKEN',
-  UPDATE_VENUE: 'UPDATE_VENUE',
-  UPDATE_CONTACT: 'UPDATE_CONTACT',
-  UPDATE_CHECKOUT: 'UPDATE_CHECKOUT',
+  UPDATE_CHECKOUT:     'UPDATE_CHECKOUT',
 }
 
 
@@ -17,8 +17,8 @@ function actionFactory(action) {
 
 
 // Actions
-export function selectPlan(id=null)       { return actionFactory(ACTIONS.SELECT_PLAN)(id) }
 export function saveVenueData(data={})    { return actionFactory(ACTIONS.UPDATE_VENUE)(data) }
 export function saveContactData(data={})  { return actionFactory(ACTIONS.UPDATE_CONTACT)(data) }
-export function saveCheckoutData(data={}) { return actionFactory(ACTIONS.UPDATE_CHECKOUT)(data) }
+export function selectPlan(id=null)       { return actionFactory(ACTIONS.SELECT_PLAN)(id) }
 export function saveStripeToken(data={})  { return actionFactory(ACTIONS.UPDATE_STRIPE_TOKEN)(data) }
+export function saveCheckoutData(data={}) { return actionFactory(ACTIONS.UPDATE_CHECKOUT)(data) }
