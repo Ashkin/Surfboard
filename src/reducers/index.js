24f4124ecc5ee8e8d7dae3cd4423711992faa4e3
Terra Bilderback <terra@itson.me>
2017-09-11 13:05:52 -0700

    DRY: Added basic reducerFactory

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,26 @@
 import { combineReducers } from 'redux'
 import { reducer as reduxFormReducer } from 'redux-form'
 
-import plans      from './plans'
-import stripe     from './stripe'
-import venue      from './venue'
-import contact    from './contact'
-import checkout   from './checkout'
+import { ACTIONS } from '../actions'
+
+
+// Basic reducer factory
+// Usage: reducerFactory(ACTIONS.WHATEVER, {default: state})
+const reducerFactory = function(actionConst, defaultState={}) {
+  return function(state=defaultState, action) {
+    if (action.type != actionConst)
+      return state;
+    return {...state, ...action.payload}
+  }
+}
+
+// Basic reduers
+const plans    = reducerFactory(ACTIONS.SELECT_PLAN)
+const stripe   = reducerFactory(ACTIONS.UPDATE_STRIPE_TOKEN)
+const venue    = reducerFactory(ACTIONS.UPDATE_VENUE)
+const contact  = reducerFactory(ACTIONS.UPDATE_CONTACT)
+const checkout = reducerFactory(ACTIONS.UPDATE_CHECKOUT)
+
 
 
 const rootReducer = combineReducers({
