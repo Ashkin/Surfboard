4ac46251baac43d8e5bd7ac1720b5bd4f255bbe8
Terra Bilderback <terra@itson.me>
2017-09-14 12:21:35 -0700

    Heroku: Exposed config vars as globals

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,5 @@
+webpack = require('webpack')
+
 module.exports = {
   entry: [
     './src/index.js'
@@ -5,6 +7,13 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      NODE_ENV:        JSON.stringify(process.env.NODE_ENV),
+      APPLICATION_KEY: JSON.stringify(process.env.APPLICATION_KEY),
+      AUTH_TOKEN:      JSON.stringify(process.env.AUTH_TOKEN)
+    })
+  ],
   output: {
     path: __dirname,
     publicPath: '/',
