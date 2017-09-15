8012d788518091bfe0e85581d56aa1ea89a71826
Terra Bilderback <terra@itson.me>
2017-09-15 11:39:04 -0700

    Renamed auth_token to onboarding_token to match the config var on drinkboard

--- webpack.common.js
+++ webpack.common.js
@@ -9,9 +9,9 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      NODE_ENV:        JSON.stringify(process.env.NODE_ENV),
-      APPLICATION_KEY: JSON.stringify(process.env.APPLICATION_KEY),
-      AUTH_TOKEN:      JSON.stringify(process.env.AUTH_TOKEN)
+      NODE_ENV:         JSON.stringify(process.env.NODE_ENV),
+      APPLICATION_KEY:  JSON.stringify(process.env.APPLICATION_KEY),
+      ONBOARDING_TOKEN: JSON.stringify(process.env.ONBOARDING_TOKEN)
     })
   ],
   output: {
