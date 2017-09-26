0ae4affd19194909da0f386ad605a48c9a977120
Terra Bilderback <terra@itson.me>
2017-09-26 16:32:04 -0700

    Fixed initialValues and validation errors on rerender.  Significant code improvements

--- src/components/venue-contact.js
+++ src/components/venue-contact.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { TextField } from 'material-ui/TextField'
+import { connect }          from 'react-redux'
 
 import { renderTextField } from '../helpers/material-ui-redux-form'
 import classBuilder        from '../helpers/class-builder'
@@ -14,45 +12,15 @@ class VenueContact extends Component {
   buildTextField(options) {
     const { name, required, label } = options
 
-    // `input_value` is a hack to make `value` work since
-    // the `renderTextField` wrapper only ever receives a
-    // blank string instead; I'm trying to figure out why
     return (
       <Field
         component={renderTextField}
         name={name}
-        className={''}
-        hintText={label + (required ? "" : " (optional)")}
-        onChange={(event) => this.handleChange(name, event.target.value)}
-        input_value={this.getValue(name)}
+        label={label + (required ? " (required)" : "")}
+        hint={label}
       />
     )
   }
-
-
-
-  //TODO: remove this and rely on redux-form's onChange instead
-  //      this will, ofc, require reading values from
-  //      `state.form.{formName}.{inputName}` or similar
-  handleChange(key, val) {
-    if (["name","position"].includes(key))
-      console.log(`[VenueContact::handleChange()]  key: ${key}  val: `, val)
-    this.props.saveContactData({[key]: val})
-  }
-
-  getValue(key) {
-    // State isn't populated yet
-    if (this.props == null)  return "ohnoes"
-
-    if (["name","position"].includes(key)) {
-      console.log(`[VenueContact::getValue(${key})] value: `, this.props.contact[key] || '')
-      console.log(" | this.props.contact: ", this.props.contact)
-    }
-
-
-    return this.props.contact[key] || ''
-  }
-
 
 
   render() {
@@ -65,13 +33,12 @@ class VenueContact extends Component {
         </header>
         <summary></summary>
         <form className="venue" onSubmit={handleSubmit(this.handleSubmit.bind(this))}>
-          { /* What a travesty. */ }
-          { this.buildTextField.bind(this, {name:"name",     required:true,  label:"Contact Name"})() }
-          { this.buildTextField.bind(this, {name:"position", required:false, label:"Contact Position"})() }
-          { this.buildTextField.bind(this, {name:"email",    required:true,  label:"Email Address"})() }
-          { this.buildTextField.bind(this, {name:"url",      required:false, label:"Website"})() }
-          { this.buildTextField.bind(this, {name:"phone",    required:false, label:"Phone Number"})() }
-          { this.buildTextField.bind(this, {name:"pos",      required:false, label:"Point-of-Sale System"})() }
+          { this.buildTextField({name:"name",     required:true,  label:"Contact Name"}) }
+          { this.buildTextField({name:"position", required:false, label:"Contact Position"}) }
+          { this.buildTextField({name:"email",    required:true,  label:"Email Address"}) }
+          { this.buildTextField({name:"url",      required:false, label:"Website"}) }
+          { this.buildTextField({name:"phone",    required:false, label:"Phone Number"}) }
+          { this.buildTextField({name:"pos",      required:false, label:"Point-of-Sale System"}) }
 
           <div className="center">
             <button type="submit" className="button">Next</button>
@@ -81,9 +48,11 @@ class VenueContact extends Component {
     )
   }
 
+
   handleSubmit(values) {
     console.log("[VenueContact] Submitting!")
     this.props.saveContactData(values)
+    console.log(" | Done")
     // .then(() => {
     //   this.props.complete()
     // })
@@ -92,36 +61,36 @@ class VenueContact extends Component {
 
 
 
-function addError(errors, key, message) {
-  errors[key] = message
-  errors.first_error = errors.first_error || key
-}
 
 function validate(values) {
   const errors = {}
+  const requiredFields = ['name', 'email']
 
-  if (!values.email)    addError(errors, "email",   "Please provide an email address")
+  requiredFields.forEach((field) => {
+    if (!values[field])
+      errors[field] = ' '  // Displays invalid styles without displaying a message
+  })
 
   return errors
 }
 
-
-
 function mapStateToProps(state) {
   const contact = state.contact || {}
+  const initialValues = contact
 
-  console.log("[VenueContact] state: ", contact)
-
-  return { contact }
+  return { contact, initialValues }
 }
 
 
 
 
-
-export default reduxForm({
+let formOptions = {
   validate,
-  form: 'ContactForm'
-})(
-  connect(mapStateToProps,{ saveContactData })(VenueContact)
+  form: 'contact',
+}
+
+export default connect(
+  mapStateToProps, { saveContactData }
+)(
+  reduxForm(formOptions)(VenueContact)
 )
