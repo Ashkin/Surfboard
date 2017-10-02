f47be6e3ba80684a2ef1bc3afdadd31dad7a397e
Terra Bilderback <terra@itson.me>
2017-10-02 15:47:22 -0700

    Converted to stateless component; considerable layout changes

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,47 +1,56 @@
-import React, { Component } from 'react'
-import Paper                from 'material-ui/Paper'
+import React from 'react'
+import Paper from 'material-ui/Paper'
 
+const Welcome = ({nextStep}) => {
+  return (
+    <section className="welcome">
+      <Paper className="paper" zDepth={2}>
+        <header>Welcome!</header>
+        <summary>
+          Creating your merchant account will only take a few minutes,
+          and you can start selling gift cards online as soon as tomorrow.
+        </summary>
 
-class Welcome extends Component {
+        <article style={{textAlign: 'left', width: '50%', margin: '0 auto'}}>
+          <br/>
+          <br/>
+          <span>Here's what we'll need from you:</span>
+          <br/><br/>
+          <Paper className="paper item">
+            Your venue's name and address
+          </Paper>
+          <Paper className="paper item">
+            Contact name and email for following up
+            <aside>This will be our primary contact on your team.</aside>
+          </Paper>
+          <Paper className="paper item">
+            A high-res logo for your venue
+            <aside>
+              The logo must be square, and should be between 150px and 1000px on a side.  (Higher-res will always look better)
+            </aside>
+          </Paper>
+          <Paper className="paper item">
+            A high-res cover photo (e.g. your front-of-house)
+            <aside>
+              The cover photo must be rectangular and landscape, and <em>at least</em> 1500px wide by 500px tall.
+            </aside>
+          </Paper>
+          <Paper className="paper item">
+            Credit card info
+            <aside>We won't bill you until we've confirmed everything with you over email.</aside>
+          </Paper>
 
-  render() {
-
-    const { nextStep } = this.props
-
-
-    return (
-      <section className="welcome">
-        <Paper className="paper" zDepth={2}>
-          <header>Welcome!</header>
-          <summary>
-            We'll get you signed up with a merchant account within a few minutes, <wbr/>
-            and selling gift cards shortly thereafter.
-          </summary>
-
-          <article style={{textAlign: 'left', width: '50%', margin: '0 auto'}}>
-            <br/>
-            <br/>
-            <p>Sound good?</p>
-
-            Here's what we'll need from you:
-            <ul>
-              <li>Venue name and address</li>
-              <li>Contact name and email (for following up)</li>
-              <li>High-res logo</li>
-              <li>High-res cover photo (e.g. your front-of-house)</li>
-              <li>Creditcard info</li>
-            </ul>
-            <aside style={{fontSize: '0.85em'}}>(And don't worry: we won't bill you until we've confirmed all of the details over email.)</aside>
-            <br/>
-            <br/>
+          <br/>
+          <br/>
+          <div className="center">
             Click the button below and let's get started!
-          </article>
+          </div>
+        </article>
 
-          <button onClick={nextStep}>Next</button>
-        </Paper>
-      </section>
-    )
-  }
+        <button onClick={nextStep}>Next</button>
+      </Paper>
+    </section>
+  )
 }
 
 export default Welcome
