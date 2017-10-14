c517ca03072375405bb5b2836026944d4dab608d
Terra Bilderback <terra@itson.me>
2017-10-13 17:15:51 -0700

    Forward http->https

--- src/server.js
+++ src/server.js
@@ -7,6 +7,13 @@ module.exports = {
     const indexPath = path.join(__dirname, "../index.html")
     const publicPath = express.static(path.join(__dirname, "..")) // ../public"))
 
+    app.use(function(request, response, next) {
+      if (!request.secure  &&  !request.headers.host.startsWith("localhost")) {
+        console.log("Insecure connection; redirecting to https")
+        response.redirect("https://" + request.headers.host + request.url)
+      } else next()
+    })
+
     app.use("/", publicPath)
     app.get("/", function (_, res) { res.sendFile(indexPath) })
 
