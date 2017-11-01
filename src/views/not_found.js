a063b620152a14cd338153ff9ea673adfde0cb4e
Terra Bilderback <terra@itson.me>
2017-10-31 18:13:21 -0700

    Better, responsive not_found view

--- src/views/not_found.js
+++ src/views/not_found.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
 
+import Paper from "material-ui/Paper"
+
 
 // Basic 404 view
 //TODO: abstract out markup to its own component, and pass message and status-code
@@ -9,18 +11,20 @@ class ViewNotFound extends Component {
 
     render() {
         return (
-            <main className="http-error">
-                <div className="wrapper">
-                    <div className="side">
-                        <h1>Oh no!</h1>
-                        <h3>You've found yourself somewhere you're not meant to be.</h3>
-                        <aside>(Error 404)</aside>
-                        <Link to="/" className="button">Back to safety</Link>
+            <main className="http-error error-404">
+                <Paper className="paper primary" zDepth={2}>
+                    <div className="overlay">
+                        <span>???</span>
                     </div>
-                    <div className="side">
-                        <div className="missing-giftcard">???</div>
+                    <div className="wrapper">
+                        <div>
+                            <h1>Oh no!</h1>
+                            <h3>You've found yourself somewhere you're not meant to be.</h3>
+                            <aside>(Error 404)</aside>
+                            <Link to="/" className="button">Back to safety</Link>
+                        </div>
                     </div>
-                </div>
+                </Paper>
             </main>
         )
     }
