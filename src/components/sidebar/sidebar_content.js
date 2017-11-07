2b9f7478ffcd426af5fc3773948c9fd564cfd467
Terra Bilderback <terra@itson.me>
2017-11-07 15:15:21 -0800

    Updated sidebar content

--- src/components/sidebar/sidebar_content.js
+++ src/components/sidebar/sidebar_content.js
@@ -1,28 +1,46 @@
-//React
-import React, { Component } from 'react'
+// React
+import React, { Component } from "react"
+
+// History
+import { withRouter } from "react-router-dom"
 
 // Mui Components
-import {List, ListItem} from 'material-ui/List'
-import Paper from 'material-ui/Paper'
+import {List, ListItem} from "material-ui/List"
+import Paper from "material-ui/Paper"
+import Divider from "material-ui/Divider"
 
 // Local files
-import Logo from '../logo'
+import Logo from "../logo"
 
 
-const SidebarContent = ({sidebarHide}) => {
-    return (
-        <List className="sidebar-content">
-            <Paper
-                rounded={false}
-                zDepth={2}
-                className="paper"
-            >
-                <Logo />
-            </Paper>
-            <ListItem onClick={sidebarHide} primaryText="List Item" />
-            <ListItem onClick={sidebarHide} primaryText="List Item 2" />
-        </List>
-    )
+class SidebarContent extends Component {
+    render() {
+        return (
+            <List className="sidebar-content">
+                <Paper
+                    rounded={false}
+                    zDepth={2}
+                    className="paper"
+                >
+                    <Logo />
+                </Paper>
+                <ListItem onClick={this.showOnboard.bind(this)}  primaryText="Merchant Signup" />
+                <ListItem onClick={this.showOrdering.bind(this)} primaryText="Ordering" secondaryText="Printers, Marketing items" />
+            </List>
+        )
+    }
+
+    // These must be bound to access history
+    showOnboard() {
+        this.props.history.push("/onboard")
+        this.props.sidebarHide()
+    }
+
+    showOrdering() {
+        this.props.history.push("/order")
+        this.props.sidebarHide()
+    }
 }
 
-export default SidebarContent
+
+export default withRouter(SidebarContent)
